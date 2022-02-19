1055389dbe9894b6ea3375da15d6cfb19bf6affc
Aroshakalpana <89211301+Aroshakalpana2468@users.noreply.github.com>
2022-02-19 19:53:04 +0530

    Update __main__.py

--- wbb/__main__.py
+++ wbb/__main__.py
@@ -110,7 +110,7 @@ home_keyboard_pm = InlineKeyboardMarkup(
                 text="❓ Commands ❓", callback_data="bot_commands"
             ),
             InlineKeyboardButton(
-                text="Support Channel",
+                text="📌Updates Channel📌",
                 url="https://t.me/szteambots",
             ),
         ],
@@ -120,12 +120,12 @@ home_keyboard_pm = InlineKeyboardMarkup(
                 callback_data="stats_callback",
             ),
             InlineKeyboardButton(
-                text="Support 👨", url="https://t.me/slbotzone"
+                text="👨 Support 👨", url="https://t.me/slbotzone"
             ),
         ],
         [
             InlineKeyboardButton(
-                text="Add Me To Your Group 🎉",
+                text="🎉Add Me To Your Group 🎉",
                 url=f"http://t.me/{BOT_USERNAME}?startgroup=new",
             )
         ],
@@ -147,7 +147,7 @@ keyboard = InlineKeyboardMarkup(
                 url=f"t.me/{BOT_USERNAME}?start=help",
             ),
             InlineKeyboardButton(
-                text="Support Channel",
+                text="📌Updates Channel📌",
                 url="https://t.me/szteambots",
             ),
         ],
@@ -156,7 +156,7 @@ keyboard = InlineKeyboardMarkup(
                 text="System Stats 💻",
                 callback_data="stats_callback",
             ),
-            InlineKeyboardButton(text="Support 👨", url="t.me/slbotZone"),
+            InlineKeyboardButton(text="👨 Support 👨", url="t.me/slbotZone"),
         ],
     ]
 )
