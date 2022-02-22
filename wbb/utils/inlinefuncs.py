3747ac6f9853e151e1c8f1e9147ee1cbd0a65f58
Aroshakalpana <89211301+Aroshakalpana2468@users.noreply.github.com>
2022-02-22 15:08:15 +0530

    Update inlinefuncs.py

--- wbb/utils/inlinefuncs.py
+++ wbb/utils/inlinefuncs.py
@@ -97,13 +97,7 @@ async def inline_help_func(__HELP__):
             ),
             thumb_url="https://telegra.ph/file/b20680b4da51d86952d95.jpg",
         ),
-        InlineQueryResultArticle(
-          title="SUPPORT GROUP",
-          description="suport group.",
-          input_message_content=InputTextMessageContent(
-            "https://t.me/SlNinjaTeam"
-          ),
-          thumb_url="https://telegra.ph/file/b20680b4da51d86952d95.jpg",
+       
             
       
     ]
